9224dc4b46281b7f8a31fee049bd747db4c0780d
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-05 18:02:56 +0530

    Add partition method to Solution class
    Implement partition function for linked list based on value x.

--- 86.py
+++ 86.py
@@ -1,1 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
+class Solution(object):
+    def partition(self, head, x):
+        """
+        :type head: Optional[ListNode]
+        :type x: int
+        :rtype: Optional[ListNode]
+        """
+        smallDummy = ListNode(0)
+        largeDummy = ListNode(0)
+        small = smallDummy
+        large = largeDummy
+        while head:
+            if head.val < x:
+                small.next = head
+                small = small.next
+            else:
+                large.next = head
+                large = large.next
+            head = head.next
+        large.next = None
+        small.next = largeDummy.next
+
+        return smallDummy.next
