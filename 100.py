483693b5d4d3a7c78d3699f1a4d783e451ac0912
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-05 18:02:03 +0530

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,10 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution(object):
     def isSameTree(self, p, q):
         """
@@ -12,15 +5,11 @@ class Solution(object):
         :type q: Optional[TreeNode]
         :rtype: bool
         """
-
         if not p and not q:
             return True
-
         if not p or not q:
             return False
-
         if p.val != q.val:
             return False
-
         return (self.isSameTree(p.left, q.left) and
                 self.isSameTree(p.right, q.right))
