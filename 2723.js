d864263dd7ffc3420e742d7560594d51de3f3f76
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-09 18:53:49 +0530

    Update 2723.js

--- 2723.js
+++ 2723.js
@@ -33,3 +33,19 @@ ArrayWrapper.prototype.toString = function() {
  * obj1 + obj2;        // 10
  * String(obj1);       // "[1,2]"
  */
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+
+    return value1 + value2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
