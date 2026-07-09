1d1e632f9942d420ccd8f591a52817fd846694ae
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-09 18:49:21 +0530

    Update calcscore.js

--- calcscore.js
+++ calcscore.js
@@ -20,6 +20,33 @@ function calculateScore(s) {
             pos[ch].push(i);
         }
     }
+function calculateScore(s) {
+    let pos = {};
+    let score = 0;
 
+    for (let i = 0; i < s.length; i++) {
+        let ch = s[i];
+
+        // Find the mirror character
+        let twin = String.fromCharCode(
+            'a'.charCodeAt(0) + 'z'.charCodeAt(0) - ch.charCodeAt(0)
+        );
+
+        if (pos[twin] && pos[twin].length > 0) {
+            let j = pos[twin].pop();
+            score += i - j;
+        } else {
+            if (!pos[ch]) {
+                pos[ch] = [];
+            }
+            pos[ch].push(i);
+        }
+    }
+
+    return score;
+}
+Example
+console.log(calculateScore("aczzx"));   // 5
+console.log(calculateScore("abcdef"));  // 0
     return score;
 }
