0c0dac9f1d76886d8d019a3c8b084e6cf9012e04
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-10 01:12:45 +0530

    Update 2665.js

--- 2665.js
+++ 2665.js
@@ -1,1 +1,23 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentValue = init;
+
+    return {
+        increment: function() {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrement: function() {
+            currentValue -= 1;
+            return currentValue;
+        },
+        reset: function() {
+            currentValue = init;
+            return currentValue;
+        }
+    };
+};
 
