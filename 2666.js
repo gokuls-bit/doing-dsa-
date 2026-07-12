9acee5a9c604ac48aa2225094bc64cfd4c875574
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-12 15:24:46 +0530

    Update 2666.js

--- 2666.js
+++ 2666.js
@@ -44,3 +44,30 @@ var once = function(fn) {
  * onceFn(1, 2, 3); // 6
  * onceFn(2, 3, 6); // undefined
  */
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+
+    let called = false;
+
+    return function(...args) {
+
+        if (called) {
+            return undefined;
+        }
+
+        called = true;
+
+        return fn(...args);
+    };
+};
+
+/**
+ * let fn = (a, b, c) => (a + b + c);
+ * let onceFn = once(fn);
+ *
+ * onceFn(1, 2, 3); // 6
+ * onceFn(2, 3, 6); // undefined
+ */
