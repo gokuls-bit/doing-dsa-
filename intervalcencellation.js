00d37535a2b73365df5bdc002f56212ce618b5f1
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-13 23:41:29 +0530

    Update intervalcencellation.js

--- intervalcencellation.js
+++ intervalcencellation.js
@@ -1,1 +1,20 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    // Execute immediately
+    fn(...args);
 
+    // Execute repeatedly every t milliseconds
+    const intervalId = setInterval(() => {
+        fn(...args);
+    }, t);
+
+    // Return cancellation function
+    return function cancelFn() {
+        clearInterval(intervalId);
+    };
+};
