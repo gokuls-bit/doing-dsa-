2526a64be97d7954ad34a04ef9a0a9a5975f67b5
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-13 23:06:21 +0530

    Update sequentialdesign.js

--- sequentialdesign.js
+++ sequentialdesign.js
@@ -1,1 +1,21 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number[]}
+ */
+var sequentialDigits = function(low, high) {
+    const digits = "123456789";
+    const result = [];
 
+    for (let length = 2; length <= 9; length++) {
+        for (let start = 0; start + length <= 9; start++) {
+            const num = Number(digits.substring(start, start + length));
+
+            if (num >= low && num <= high) {
+                result.push(num);
+            }
+        }
+    }
+
+    return result.sort((a, b) => a - b);
+};
