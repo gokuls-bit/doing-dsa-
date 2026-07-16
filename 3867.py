2d4d6931d7bafa1ba0a65ef0c5be08ae23ad3bfe
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-16 22:23:14 +0530

    Update 3867.py

--- 3867.py
+++ 3867.py
@@ -30,3 +30,38 @@ class Solution(object):
             right -= 1
 
         return answer
+        from math import gcd
+
+class Solution:
+    def gcdSum(self, nums: list[int]) -> int:
+
+        n = len(nums)
+
+        prefixGcd = []
+
+        currentMax = 0
+
+        for num in nums:
+
+            currentMax = max(currentMax, num)
+
+            prefixGcd.append(gcd(num, currentMax))
+
+        prefixGcd.sort()
+
+        left = 0
+        right = n - 1
+
+        answer = 0
+
+        while left < right:
+
+            answer += gcd(
+                prefixGcd[left],
+                prefixGcd[right]
+            )
+
+            left += 1
+            right -= 1
+
+        return answer
